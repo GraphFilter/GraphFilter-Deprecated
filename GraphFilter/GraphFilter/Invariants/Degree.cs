0a65a01f51fa6a26aa28db56ec5f9f3a04f3eb2e
atilaajones <41593241+atilaajones@users.noreply.github.com>
2019-11-08 14:47:09 -0300

    Criação dos invariantes relacionados à grau. Criação da classe ListOfInvariants para serem usadas no combobox

---
+++ GraphFilter/GraphFilter/Invariants/Degree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphFilter.Invariants
+{
+    public static class Degree
+    {
+        public static int Max(Graph g)
+        {
+            return g.sequenceDegree.Max();
+        }
+
+        public static int Min(Graph g)
+        {
+            return g.sequenceDegree.Min();
+        }
+
+        public static double Average(Graph g)
+        {
+            return g.sequenceDegree.Average();
+        }
+    }
+}
---
+++ GraphFilter/GraphFilter/Invariants/Spectral/Spectral.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphFilter.Invariants.Spectral
+{
+    public static class Spectral
+    {
+
+    }
+}
