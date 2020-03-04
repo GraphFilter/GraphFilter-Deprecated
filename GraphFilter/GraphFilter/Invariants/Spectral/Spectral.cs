a28d62585b29e0364240b3182cd988c7f126aafa
atilaajones <41593241+atilaajones@users.noreply.github.com>
2020-03-04 18:37:10 -0300

    Criação da Matriz Laplaciana e Laplaciana sem sinal Instalação do pacote AlgLib pelo NuGet

--- GraphFilter/GraphFilter/Invariants/Spectral/Spectral.cs
+++ GraphFilter/GraphFilter/Invariants/Spectral/Spectral.cs
@@ -8,6 +8,55 @@ namespace GraphFilter.Invariants.Spectral
 {
     public static class Spectral
     {
+        private static int[,] AdjacencyMatrix(Graph g)
+        {
+            return g.adjacencyMatrix;
+        }
+        
+        private static int[,] LaplacianMatrix(Graph g)
+        {
+            int[,] laplacian = new int[g.order, g.order];
+            for (int i = 0; i < g.order; i++)
+            {
+                for (int j = i; j < g.order; j++)
+                {
+                    if (i != j)
+                    {
+                        laplacian[i, j] = -g.adjacencyMatrix[i, j];
+                        laplacian[j, i] = -g.adjacencyMatrix[i, j];
+                    }
+                    else
+                    {
+                        laplacian[i, j] = g.DegreeOf(i);
+                    }                    
+                }
+            }
+            return laplacian;
+        }
 
+        private static int[,] SignlessLaplacianMatrix(Graph g)
+        {
+            int[,] laplacian = new int[g.order, g.order];
+            for (int i = 0; i < g.order; i++)
+            {
+                for (int j = i; j < g.order; j++)
+                {
+                    if (i != j)
+                    {
+                        laplacian[i, j] = g.adjacencyMatrix[i, j];
+                        laplacian[j, i] = g.adjacencyMatrix[i, j];
+                    }
+                    else
+                    {
+                        laplacian[i, j] = g.DegreeOf(i);
+                    }
+                }
+            }
+            return laplacian;
+        }
+        public algebraicConnectivity(Graph g)
+        {
+           
+        }
     }
 }
