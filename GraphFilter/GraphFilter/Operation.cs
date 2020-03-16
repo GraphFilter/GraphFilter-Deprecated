937bd1489dcaaa877f4a5219abbf6131d467e30e
atilaajones <41593241+atilaajones@users.noreply.github.com>
2020-03-16 19:03:33 -0300

    Preparação para implementação das Operações. Atividade postada no Trello dia 16/03/20

---
+++ GraphFilter/GraphFilter/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphFilter
+{
+    public static class Operation
+    {
+        public static Graph Complement(Graph g)
+        {
+            //deve retornar um Grafo complemento do grafo de entrada
+            return g;
+        }
+
+        public static Graph LineGraph(Graph g)
+        {
+            //deve retornar um Grafo linha do grafo de entrada
+            return g;
+        }
+
+
+    }
+}
