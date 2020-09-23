90d385b7de19b7983d2137cd45ad4aa974151452
Átila Arueira Jones <atila.jones@ifsudestemg.edu.br>
2020-09-23 18:28:23 -0300

    Upgrade sharpcompress to 0.21.0 in GraphFilter/packages.config and GraphFilterTest/packages.config

--- GraphFilter/GraphFilter/Properties/AssemblyInfo.cs
+++ GraphFilter/GraphFilter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("FilterGraphs")]
+[assembly: AssemblyTitle("GraphFilter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FilterGraphs")]
+[assembly: AssemblyProduct("GraphFilter")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
